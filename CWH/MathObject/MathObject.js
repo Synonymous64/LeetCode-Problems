20c2e685feafb4a8c554ebb2109b0ac968dc24a9
Synonymous64 <prajwalurkude007@gmail.com>
2022-06-20 09:29:51 +0530

    about math objects in JS

---
+++ CWH/MathObject/MathObject.js
@@ -0,0 +1,16 @@
+console.log('====================================');
+console.log("Exploring the Math-object");
+console.log('====================================');
+let c = 6;
+let y = 4;
+let z = Math;
+z = Math.PI;
+z = Math.round(4.1);
+z = Math.ceil(4.1);
+console.log(z);
+z = Math.floor(4.1);
+z = Math.abs(-4.90); // it will make the number positive removing the redundant zero's making and decimals it absolute
+z = 100* Math.random(); // will go zero to 100;
+z = 50 + (100 - 50) *Math.random();
+z = Math.ceil(50 + (100 - 50) *Math.random());
+console.log(z);
