a3f7dd702f60e579cd57b5db1c009e58269ea2d2
Synonymous64 <prajwalurkude007@gmail.com>
2022-07-25 23:30:34 +0530

    Operators

---
+++ LatestJavaScript/conditonaloperators.js
@@ -0,0 +1,18 @@
+console.log("Operators in JavaScript");
+console.log("Arithmetic Operators in JavaScript");
+let a = 10;
+let b = 3;
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b); //* Exponential
+/*
+11
+11
+11
+11
+10
+10
+*/
